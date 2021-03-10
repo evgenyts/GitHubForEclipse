16a2f763375eefbbec2a95d5fa3a2b6ec59f686e
evgenyts <Evgeny.Tseitlin@rci.rogers.com>
2021-03-10 15:23:31 -0500

    p

--- src/datatypes/Employee.java
+++ src/datatypes/Employee.java
@@ -3,10 +3,6 @@ package datatypes;
 public class Employee extends Person {
 	String role;
 	int yearsOfExpirience;
-<<<<<<< HEAD
-	double salaryProject;
-=======
 	double salaryProjectAA;
->>>>>>> refs/remotes/origin/R621-projectA
 
 }
